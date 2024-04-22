2d62960e9b8b07d76bee2bb9cce6909f8234a58c
ketan75vekariya <ketan75vekariya@yahoo.com>
2024-04-22 17:39:37 +0100

    First commit

---
+++ src/main/java/com/restfulapiforphotoalbum/restfulapiforphotoalbum/RestfulapiforphotoalbumApplication.java
@@ -0,0 +1,13 @@
+package com.restfulapiforphotoalbum.restfulapiforphotoalbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulapiforphotoalbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulapiforphotoalbumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restfulapiforphotoalbum/restfulapiforphotoalbum/RestfulapiforphotoalbumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restfulapiforphotoalbum.restfulapiforphotoalbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulapiforphotoalbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
